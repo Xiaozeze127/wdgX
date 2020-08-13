5108be7d8527376a28cb4eea4a3bb93a02a3dbc1
一只小泽泽 <7500788+Maftert@user.noreply.gitee.com>
2020-08-14 00:59:39 +0800

    First update

---
+++ Create-Webpage.py
@@ -0,0 +1,161 @@
+import os
+import time
+
+path = "mirrors/"  # Folder Path
+printprogressbar = False # progress bar (beta)
+t_html = "mirrors/dhtml/t.html" # t_html path(Please read READEME.md)
+d_html = "mirrors/dhtml/d.html" # d_html path(Please read READEME.md)
+
+# Init
+filenum = 0
+jd = 0
+
+
+def gci(filepath):
+    files = os.listdir(filepath)
+    for fi in files:
+        fi_d = os.path.join(filepath, fi)
+        if os.path.isdir(fi_d):
+            global filenum
+            filenum = filenum + 1
+            gci(fi_d)
+        else:
+            filenum = filenum + 1
+    return filenum
+
+
+def chtml(basepath):
+    global jd
+    for item in sorted(os.listdir(basepath)):
+        path = os.path.join(basepath, item)
+        if (('index.html' in path) or ('git' in path) or ('CNAME' in path) or ('.DS_Store' in path) or ('README.md' in path) or ('img' in path) or ('dhtml' in path) or ('json' in path) or ('js' in path) or ('css' in path)):  # Folder to mask
+            continue
+        elif os.path.isfile(path):
+            modifiedTime = time.localtime(os.stat(path).st_mtime)
+            mTime = time.strftime('%Y-%m-%d %H:%M:%S', modifiedTime)
+            indexhtml = open(os.path.dirname(
+                path)+"/index.html", "a+", encoding='UTF-8')
+            indexhtml.write(
+                "<tr class=\"tbody\"><td><a class=\"tname\" href=\""+str(os.path.basename(path)).replace(' ', '%20')+"\">"+str(os.path.basename(path))+"</a></td>"+"<td><a class=\"tt\">"+mTime+"</a></td></tr>")
+            jd = jd + 1
+            if printprogressbar:
+                print("<", end='')
+                for i in range(0, int((jd * 100 / 3) / filenum)):
+                    print("*", end='')
+                for i in range(int((jd * 100 / 3) / filenum), 100):
+                    print(" ", end='')
+                print(">    ", end=' ')
+                print(str(int((jd * 100 / 3) / filenum)) + "%   ", end=' ')
+                print(str(jd) + "/" + str(filenum), end='\r')
+        else:
+            modifiedTime = time.localtime(os.stat(path).st_mtime)
+            mTime = time.strftime('%Y-%m-%d %H:%M:%S', modifiedTime)
+            indexhtml = open(os.path.dirname(
+                path)+"/index.html", "a+", encoding='UTF-8')
+            indexhtml.write(
+                "<tr class=\"tbody\"><td><a class=\"tname\" href=\""+str(os.path.basename(path)).replace(' ', '%20')+"\">"+str(os.path.basename(path))+"</a></td>"+"<td><a class=\"tt\">"+mTime+"</a></td></tr>")
+            indexhtml.close
+            jd = jd + 1
+            if printprogressbar:
+                print("<", end='')
+                for i in range(0, int((jd * 100 / 3) / filenum)):
+                    print("*", end='')
+                for i in range(int((jd * 100 / 3) / filenum), 100):
+                    print(" ", end='')
+                print(">    ", end=' ')
+                print(str(int((jd * 100 / 3) / filenum)) + "%   ", end=' ')
+                print(str(jd) + "/" + str(filenum), end='\r')
+            chtml(path)
+
+
+def htop(basepath):
+    global jd
+    for item in sorted(os.listdir(basepath)):
+        path = os.path.join(basepath, item)
+        if (('index.html' in path) or ('git' in path) or ('CNAME' in path) or ('.DS_Store' in path) or ('README.md' in path) or ('img' in path) or ('dhtml' in path) or ('json' in path) or ('js' in path) or ('css' in path)):  # Folder to mask
+            continue
+        elif os.path.isfile(path):
+            indexhtml = open(os.path.dirname(
+                path)+"/index.html", "w+", encoding='UTF-8')
+            thtml = open(t_html, "r+", encoding='UTF-8')
+            indexhtml.write(thtml.read())
+            if not("mirrors/index.html" == os.path.dirname(path)+"/index.html"):
+                indexhtml.write(
+                    "<tr class=\"tbody\"><td><a class=\"tname\" href=\"..\">Parent directory/</a></td>"+"<td><a class=\"tt\">"+"-"+"</a></td></tr>")
+            thtml.close
+            indexhtml.close
+            jd = jd + 1
+            if printprogressbar:
+                print("<", end='')
+                for i in range(0, int((jd * 100 / 3) / filenum)):
+                    print("*", end='')
+                for i in range(int((jd * 100 / 3) / filenum), 100):
+                    print(" ", end='')
+                print(">    ", end=' ')
+                print(str(int((jd * 100 / 3) / filenum)) + "%   ", end=' ')
+                print(str(jd) + "/" + str(filenum), end='\r')
+        else:
+            indexhtml = open(os.path.dirname(
+                path)+"/index.html", "w+", encoding='UTF-8')
+            thtml = open(t_html, "r+", encoding='UTF-8')
+            indexhtml.write(thtml.read())
+            if not(os.path.dirname(path)+"/index.html" == "mirrors/index.html"):
+                indexhtml.write(
+                    "<tr class=\"tbody\"><td><a class=\"tname\" href=\"..\">Parent directory/</a></td>"+"<td><a class=\"tt\">"+"-"+"</a></td></tr>")
+            indexhtml.close
+            thtml.close
+            jd = jd + 1
+            if printprogressbar:
+                print("<", end='')
+                for i in range(0, int((jd * 100 / 3) / filenum)):
+                    print("*", end='')
+                for i in range(int((jd * 100 / 3) / filenum), 100):
+                    print(" ", end='')
+                print(">    ", end=' ')
+                print(str(int((jd * 100 / 3) / filenum)) + "%   ", end=' ')
+                print(str(jd) + "/" + str(filenum), end='\r')
+            htop(path)
+
+
+def hend(basepath):
+    global jd
+    for item in sorted(os.listdir(basepath)):
+        path = os.path.join(basepath, item)
+        if 'index.html' in path:
+            indexhtml = open(os.path.dirname(
+                path+'/index.html'), "a+", encoding='UTF-8')
+            dhtml = open(d_html, "r+", encoding='UTF-8')
+            indexhtml.write(dhtml.read())
+            indexhtml.close
+            dhtml.close
+            jd = jd + 1
+            if printprogressbar:
+                print("<", end='')
+                for i in range(0, int((jd * 100 / 3) / filenum)):
+                    print("*", end='')
+                for i in range(int((jd * 100 / 3) / filenum), 100):
+                    print(" ", end='')
+                print(">    ", end=' ')
+                print(str(int((jd * 100 / 3) / filenum)) + "%   ", end=' ')
+                print(str(jd) + "/" + str(filenum), end='\r')
+        elif not(os.path.isfile(path)):
+            jd = jd + 1
+            if printprogressbar:
+                print("<", end='')
+                for i in range(0, int((jd * 100 / 3) / filenum)):
+                    print("*", end='')
+                for i in range(int((jd * 100 / 3) / filenum), 100):
+                    print(" ", end='')
+                print(">    ", end=' ')
+                print(str(int((jd * 100 / 3) / filenum)) + "%   ", end=' ')
+                print(str(jd) + "/" + str(filenum), end='\r')
+            hend(path)
+
+
+gci(path)
+htop(path)
+print("Successful 1/3                                                                                                                                    ", end="\n")
+chtml(path)
+print("Successful 2/3                                                                                                                                    ", end="\n")
+hend(path)
+print("Successful 3/3                                                                                                                                    ", end="\n")
